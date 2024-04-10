d100e8a7a3b0d284d102fd4deb2ac1c2a141bb23
pedro-chiappani <pepeelchapa@gmail.com>
2024-04-10 18:23:34 -0300

    primer commit

---
+++ src/main/java/unlp/info/bd2/repositories/ToursRepository.java
@@ -0,0 +1,9 @@
+package unlp.info.bd2.repositories;
+
+import unlp.info.bd2.model.User;
+
+public interface ToursRepository {
+
+    User createUser();
+
+}
---
+++ src/main/java/unlp/info/bd2/repositories/ToursRepositoryImpl.java
@@ -0,0 +1,13 @@
+package unlp.info.bd2.repositories;
+
+import unlp.info.bd2.model.User;
+
+public class ToursRepositoryImpl implements ToursRepository {
+
+    @Override
+    public User createUser() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+}
